ee9a213d02bb185515806b321189d4ab2371e06f
lahcen BENCHAREF <hassanebencharef@gmail.com>
2022-12-23 20:04:40 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
@@ -6,27 +6,20 @@ import Skill from "./components/skills/Skill";
 import Labs from "./components/labs/Labs";
 import Footer from "./components/footer/Footer";
 import Comments from "./components/comments/Comments";
-import RegisterLogin from "../src/components/auth/RegisterLogin";
 function App() {
-  const [found,setFound]=useState(false)
+ 
   return (
    
     <div>
-      {
-        found ?<>
+      
         <Navbar />
-        <Home setFound={setFound} />
+        <Home />
         <About />
         <Skill />
         <Labs />
         <Comments />
         <Footer />
-        </> :
       
-          <RegisterLogin setFound={setFound} found={found} />
-      
-        
-      }
     
     </div>
   );
